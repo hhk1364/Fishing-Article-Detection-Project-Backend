f94cd0cef50142192b4f0337ddc6e13477494ae7
hyunkyung han <hhg0615@naver.com>
2023-07-05 14:28:02 +0900

    HTTPS 설정

--- api_server/settings.py
+++ api_server/settings.py
@@ -12,6 +12,15 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+#  HTTPS를 통해 리버스 프록시 서버를 사용하는 경우, 프록시 서버가 HTTPS를 감지하도록 설정
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+#  모든 HTTP 요청을 HTTPS로 리디렉션
+SECURE_SSL_REDIRECT = True
+#  SESSION_COOKIE_SECURE 및 CSRF_COOKIE_SECURE: 세션 쿠키와 CSRF 토큰 쿠키를 HTTPS로만 전송하도록 설정
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
