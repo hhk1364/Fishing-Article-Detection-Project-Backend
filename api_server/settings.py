e133a83aee08a9fb3d08e956032df83becd4d8cd
hyunkyung han <hhg0615@naver.com>
2023-07-05 13:00:03 +0900

    DEBUG모드 False

--- api_server/settings.py
+++ api_server/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'a=9^b5f+q^7*8xv9kz&2=o#mddn=#3o775%_r)2!x$@z8*=!+@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     ".ap-northeast-2.compute.amazonaws.com"
